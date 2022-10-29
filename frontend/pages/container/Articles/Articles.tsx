7cd4deb3a411a70287624c53c0f3cf79833c2ce1
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-10-29 19:30:56 +0200

    TODO ADD GUI

--- frontend/pages/container/Articles/Articles.tsx
+++ frontend/pages/container/Articles/Articles.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { backEndClient, urlFor } from "../../../sanityclients";
 import Image from "next/image";
-import Markdown from 'markdown-to-jsx';
-
-
+import Markdown from "markdown-to-jsx";
 
 export default function Articles() {
   const [articles, setArticles] = useState([]);
@@ -17,17 +15,17 @@ export default function Articles() {
   return (
     <div>
       {articles.map((article: any, index: any) => {
-
-const result = `${article?.articleBody}`;
+        const result = `${article?.articleBody}`;
 
         return (
-<React.Fragment  key={index}>
-<h1>{article?.articleTitle}</h1>
-<div id="articleContent">
-</div>
-render(<Markdown>{ `${article?.articleBody}`}</Markdown>, document.body);
-</React.Fragment>           
-        )
+          <React.Fragment key={index}>
+            <h1>{article?.articleTitle}</h1>
+            <div id="articleContent"></div>
+            <h2>Description</h2>
+            <Markdown>{result.slice(0, 250)}</Markdown>
+            <hr />
+          </React.Fragment>
+        );
       })}
     </div>
   );
