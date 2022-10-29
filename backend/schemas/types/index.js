fd3420042e06856120c120a1764f0cf20fe863af
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-10-30 01:51:46 +0200

    REMOVING WARNIGNS AND ERROS FOR  FIRST DEPLOYMENT

--- backend/schemas/types/index.js
+++
@@ -1,3 +0,0 @@
-export { default as author } from "./author.js";
-export { default as categories } from "./categories.js";
-export { default as article } from "./article.js";
--- frontend/pages/components/Nav/Nav.tsx
+++ frontend/pages/components/Nav/Nav.tsx
@@ -1,28 +1,19 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Nav = () => {
   return (
-    <div >
-        <ul>
-          <Link href="/">
-            <li>Home</li>
-         
-          </Link>
-
-          <Link href="/articles">
-            <li>Articles</li>
-         
-          </Link>
-{/* Categories Drop Down Menu */}
-          <button >
-            <li> Categories</li>
-          </button> 
-
-
-        </ul>
+    <div>
+      <ul>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/articles">
+          <li>Articles</li>
+        </Link>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
