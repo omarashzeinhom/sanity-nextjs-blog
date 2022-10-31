e9b6d0c528ab164a9662e551af76b5deb1229d1f
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-10-31 03:14:42 +0200

    TODO CONTINUE WHENEVER BACK

---
+++ frontend/pages/container/[slug].tsx
@@ -0,0 +1,19 @@
+type ArticleProps = {
+    title: string,
+    description: string,
+    imageLink: string,
+    
+}
+
+
+
+export default function Article ({}){
+
+
+return (
+    <>
+    
+    </>
+)
+
+}
